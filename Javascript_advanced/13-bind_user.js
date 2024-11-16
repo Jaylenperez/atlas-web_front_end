44b60a061d85b626a2c01f3d3664557990c57c17
jaylenperez <jaylenestella24@gmail.com>
2024-11-16 16:30:55 -0600

    bind logWelcomeUser to user object and log personalized message

---
+++ Javascript_advanced/13-bind_user.js
@@ -0,0 +1,18 @@
+const user = {
+    hobby: "Calligraphy",
+    favoriteSport: "Hockey",
+    astrologicalSign: "Aries",
+    firstName: "Guillaume",
+    lastName: "Johns",
+    location: "Netherlands",
+    occupation: "Engineer"
+};
+
+function logWelcomeUser(welcomeString) {
+    console.log(`${welcomeString}, ${this.firstName}. Your occupation is: ${this.occupation}`);
+}
+
+const bindLogWelcomeUser = logWelcomeUser.bind(user);
+
+// Example usage:
+bindLogWelcomeUser("Welcome"); // Output: Welcome, Guillaume. Your occupation is: Engineer
