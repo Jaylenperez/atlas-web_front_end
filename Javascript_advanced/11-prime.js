a3309a2b5d68eb7789175bb26790795284bf21a7
jaylenperez <jaylenestella24@gmail.com>
2024-11-16 16:10:07 -0600

    defer countPrimeNumbers execution using setTimeout

---
+++ Javascript_advanced/11-prime.js
@@ -0,0 +1,26 @@
+function countPrimeNumbers() {
+    let count = 0;
+    for (let num = 2; num <= 100; num++) {
+        let isPrime = true;
+        for (let i = 2; i <= Math.sqrt(num); i++) {
+            if (num % i === 0) {
+                isPrime = false;
+                break;
+            }
+        }
+        if (isPrime) {
+            count++;
+        }
+    }
+    return count;
+}
+
+console.time('Execution time');
+
+setTimeout(() => {
+    for (let i = 0; i < 100; i++) {
+        countPrimeNumbers();
+    }
+    console.timeEnd('Execution time');
+}, 0);
+                                                                                                  
